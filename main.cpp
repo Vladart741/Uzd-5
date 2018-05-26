fb3c291441f56318ca84be6cf6fe3ddadf5e21c3
Vladyslav Palkevič <vladyslav.palkevic@mif.stud.vu.lt>
2018-05-26 18:12:22 +0300

    Done, bet be unit testingo

---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+
+struct parametrai
+{
+	int kiek;
+	std::vector<int>eil;
+};
+
+void remove(std::string& str, char character);
+std::vector<int> eilutes_nustatymas(std::string str);
+
+int main()
+{
+	std::vector<char>simboliai;
+	{
+		simboliai.push_back('.');
+		simboliai.push_back(',');
+		simboliai.push_back('!');
+		simboliai.push_back('?');
+		simboliai.push_back('=');
+		simboliai.push_back('-');
+		simboliai.push_back('"');
+		simboliai.push_back('(');
+		simboliai.push_back(')');
+		simboliai.push_back('_');
+		simboliai.push_back('+');
+		simboliai.push_back('/');
+		simboliai.push_back('*');
+		simboliai.push_back('<');
+		simboliai.push_back('>');
+		simboliai.push_back('{');
+		simboliai.push_back('}');
+		simboliai.push_back('|');
+		simboliai.push_back('#');
+		simboliai.push_back('”');
+		simboliai.push_back('“');
+		simboliai.push_back('—');
+		simboliai.push_back(':');
+		simboliai.push_back(';');
+		
+	}
+
+	std::ifstream fd("Duomenys.txt");
+	if (!fd)
+	{
+		std::cout << "Error opening data file " << std::endl;
+		return 1;
+	}
+	std::map<std::string, parametrai> duom;
+	std::string zodis;
+	while (fd >> zodis)
+	{
+		
+		for (auto i = 0; i != simboliai.size(); i++)
+		{
+			remove(zodis, simboliai[i]);
+		}
+
+		if (duom.find(zodis) != duom.end())
+		{
+			duom[zodis].kiek++;
+			duom[zodis].eil = eilutes_nustatymas(zodis);
+		}
+		else
+		{
+			duom[zodis].kiek = 1;
+		}
+		
+	}
+	duom.erase("");
+
+	std::ofstream fr("rez.txt");
+	for (auto i : duom)
+	{
+		if (i.second.kiek > 1)
+		{
+			fr << "Zodis " <<"["<< i.first<<"]" << " pasikartoja " << i.second.kiek << " kartu siose eilutese: ";
+			for (auto j = 0; j != i.second.eil.size(); j++)
+			{
+				fr << i.second.eil[j] << " ";
+			}
+			fr << std::endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+void remove(std::string& str, char character)
+{
+ size_t pos;
+ while( (pos=str.find(character)) != std::string::npos )
+      str.erase(pos, 1);
+
+}
+std::vector<int> eilutes_nustatymas(std::string str)
+{
+	std::ifstream fd("Duomenys.txt");
+	std::string zodis;
+	std::vector<int>eil;
+	int eilute = 1;
+	while (std::getline(fd, zodis))
+	{
+		if (zodis.find(str) != std::string::npos) {
+			eil.push_back(eilute);
+		}
+		eilute++;
+	}
+	return eil;
+}
